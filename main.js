73dfbc1f9c8f0c06ec0ff0463c219882584ecb61
ibletz <ibletz@gmail.com>
2021-09-19 18:35:26 -0700

    finished assignment

---
+++ main.js
@@ -0,0 +1,51 @@
+/* Ira Bletz-Fuller
+     Assignment 01 2021/09/18 */
+
+// Question 01
+let myNumber;
+
+// Question 02
+myNumber = 5;
+console.log("Type of myNumber: " + typeof(myNumber));
+console.log("myNumber = " + myNumber);
+
+// Question 03
+let myOtherNumber = 5;
+console.log("\nType of myOtherNumber: " + typeof(myOtherNumber));
+console.log("myOtherNumber = " + myOtherNumber);
+
+// Question 04
+let myTotal = myNumber * myOtherNumber;
+console.log("\nType of myTotal: " + typeof(myTotal));
+console.log("myTotal = " + myTotal);
+
+//Question 05
+let myName = "Ira Bletz-Fuller";
+let myCombo = myNumber + myOtherNumber + "";
+console.log("\nType of myCombo: " + typeof(myCombo));
+console.log("myCombo = " + myCombo);
+
+//Question 06
+let myOtherCombo = myNumber + myOtherNumber;
+console.log("\nType of myOtherCombo: " + typeof(myOtherCombo));
+console.log("myOtherCombo = " + myOtherCombo);
+
+//Question 07
+let myRemainder = myNumber % 5;
+console.log("\nType of myRemainder: " + typeof(myRemainder));
+console.log("myRemainder = " + myRemainder);
+
+//Question 08
+let i = 2;
+console.log("\nType of i: " + typeof(i));
+console.log("myRemainder = " + i);
+
+//Question 09
+let int = 4;
+console.log("\nType of int: " + typeof(int));
+console.log("int = " + int);
+
+//Question 10
+let myBoolean = false;
+console.log("\nType of myBoolean: " + typeof(myBoolean));
+console.log("myBoolean = " + myBoolean);
